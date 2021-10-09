8191449a8bf5cddea7b8486d839cd4a6b0e13993
sohaamutsaddi <38124963+sohaamutsaddi@users.noreply.github.com>
2021-10-09 14:51:52 -0400

    Integrated db with html
    books.html page is displaying data from the books table

--- app/public/js/index.js
+++ app/public/js/index.js
@@ -1,46 +1,33 @@
 const SomeApp = {
-  data() {
-    return {
-      students: [],
-      selectedStudent: null,
-      offers: []
+    data() {
+      return {
+        students: [],
+      }
+    },
+    computed: {},
+    methods: {
+        prettyDollar(n) {
+            const d = new Intl.NumberFormat("en-US").format(n);
+            return "$ " + d;
+        },
+        
+        fetchStudentData() {
+            fetch('/api/books/')
+            .then( response => response.json() )
+            .then( (responseJson) => {
+                console.log(responseJson);
+                console.log("here")
+                this.students = responseJson;
+            })
+            .catch( (err) => {
+                console.error(err);
+            })
+        },
+    },
+    created() {
+        this.fetchStudentData();
     }
-  },
-  computed: {},
-  methods: {
-      prettyData(d) {
-          return dayjs(d)
-          .format('D MMM YYYY')
-      },
-      prettyDollar(n) {
-          const d = new Intl.NumberFormat("en-US").format(n);
-          return "$ " + d;
-      },
-      selectStudent(s) {
-          if (s == this.selectedStudent) {
-              return;
-          }
-          this.selectedStudent = s;
-          this.offers = [];
-          this.fetchOfferData(this.selectedStudent);
-      },
-      fetchStudentData() {
-          fetch('/api/books/')
-          .then( response => response.json() )
-          .then( (responseJson) => {
-              console.log(responseJson);
-              this.students = responseJson;
-          })
-          .catch( (err) => {
-              console.error(err);
-          })
-      },
-
-  },
-  created() {
-      this.fetchStudentData();
+  
   }
-
-}
-
-Vue.createApp(SomeApp).mount('#offerApp');
+  
+  Vue.createApp(SomeApp).mount('#offerApp');
