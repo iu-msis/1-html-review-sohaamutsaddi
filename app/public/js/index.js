b6a276096b22bd2d05c2580f23bedca500002396
sohaamutsaddi <38124963+sohaamutsaddi@users.noreply.github.com>
2021-10-17 12:10:34 -0400

    Added form submit

--- app/public/js/index.js
+++ app/public/js/index.js
@@ -1,33 +1,62 @@
 const SomeApp = {
-    data() {
-      return {
-        students: [],
-      }
-    },
-    computed: {},
-    methods: {
-        prettyDollar(n) {
-            const d = new Intl.NumberFormat("en-US").format(n);
-            return "$ " + d;
-        },
-        
-        fetchStudentData() {
-            fetch('/api/books/')
-            .then( response => response.json() )
-            .then( (responseJson) => {
-                console.log(responseJson);
-                console.log("here")
-                this.students = responseJson;
-            })
-            .catch( (err) => {
-                console.error(err);
-            })
-        },
-    },
-    created() {
-        this.fetchStudentData();
+  data() {
+    return {
+      students: [],
+      offerForm: {}
     }
+  },
+  computed: {},
+  methods: {
+      prettyData(d) {
+          return dayjs(d)
+          .format('D MMM YYYY')
+      },
+      prettyDollar(n) {
+          const d = new Intl.NumberFormat("en-US").format(n);
+          return "$ " + d;
+      },
   
+      fetchStudentData() {
+          fetch('/api/books/')
+          .then( response => response.json() )
+          .then( (responseJson) => {
+              console.log(responseJson);
+              this.students = responseJson;
+          })
+          .catch( (err) => {
+              console.error(err);
+          })
+      },
+      
+      postNewOffer(evt) {
+        // this.offerForm.studentId = this.selectedStudent.id;        
+        console.log("Posting:", this.offerForm);
+        // alert("Posting!");
+
+        fetch('api/books/create.php', {
+            method:'POST',
+            body: JSON.stringify(this.offerForm),
+            headers: {
+              "Content-Type": "application/json; charset=utf-8"
+            }
+          })
+          .then( response => response.json() )
+          .then( json => {
+            console.log("Returned from post:", json);
+            // TODO: test a result was returned!
+            this.offers = json;
+            
+            // reset the form
+            this.offerForm = {};
+            console.log("form reset");
+            this.fetchStudentData()
+          });
+      }
+  },
+  created() {
+      this.fetchStudentData();
   }
-  
-  Vue.createApp(SomeApp).mount('#offerApp');
+
+}
+
+Vue.createApp(SomeApp).mount('#offerApp');
